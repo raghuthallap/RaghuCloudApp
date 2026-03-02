727178f119d21500a44b3288de8b0afc40ca7303
Raghuveer <raghuthallapragada@yahoo.com>
2026-03-02 17:06:42 +0530

    Initial clean commit - RaghuCloudApp

---
+++ RaghuCloudApp/Controllers/HealthController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RaghuCloudApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Healthy");
+        }
+
+        [HttpGet("info")]
+        public IActionResult GetInfo()
+        {
+            var info = new
+            {
+                Application = "RaghuCloudApp",
+                Version = "1.0",
+                Environment = Environment.MachineName,
+                ServerTime = DateTime.UtcNow
+            };
+
+            return Ok(info);
+        }
+    }
+}
